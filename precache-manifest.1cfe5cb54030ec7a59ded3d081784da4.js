93eb9670015bdbad3cea1dedbbcd926d2002591b
Chris Kim <chrisdjkim@gmail.com>
2020-06-22 03:05:59 -0400

    Updates

---
+++ precache-manifest.1cfe5cb54030ec7a59ded3d081784da4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d666466a4925dfd4d244fd5be6169272",
+    "url": "/juniordj428/aesthetic-wares/index.html"
+  },
+  {
+    "revision": "84c09429850d0e4ae5b1",
+    "url": "/juniordj428/aesthetic-wares/static/css/main.f71bf111.chunk.css"
+  },
+  {
+    "revision": "b2baf73bdea35e31977c",
+    "url": "/juniordj428/aesthetic-wares/static/js/2.5b3a401d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/juniordj428/aesthetic-wares/static/js/2.5b3a401d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84c09429850d0e4ae5b1",
+    "url": "/juniordj428/aesthetic-wares/static/js/main.86f40ffd.chunk.js"
+  },
+  {
+    "revision": "3718a340cb0268283f87",
+    "url": "/juniordj428/aesthetic-wares/static/js/runtime-main.fb55310c.js"
+  },
+  {
+    "revision": "02d83af6d0ce3633e7c2cc1059a70472",
+    "url": "/juniordj428/aesthetic-wares/static/media/bowl.02d83af6.svg"
+  }
+]);
